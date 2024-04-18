4d746beb1a61626428565c61bce8a374d93ea227
jemmert <jozemel5@gmail.com>
2024-04-17 21:05:16 -0300

    Se crearon las primeras rutas del navegador y un footer basico, se le agregaron estilos a los botones de login y sign in

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NavBar from "./Components/Navbar/NavBar";
+import Footer from "./Components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import Admin from "./Components/Pages/Admin";
+import Error404 from "./Components/Pages/Error404";
+import Contact from "./Components/Pages/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/error" element={<Error404/>} />
+            <Route path="/admin" element={<Admin/>} />
+          </Routes>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Pages/Error404.jsx
@@ -0,0 +1,11 @@
+
+
+const Error404 = () => {
+    return (
+        <div>
+            <h1>desde error404</h1>
+        </div>
+    );
+};
+
+export default Error404;
