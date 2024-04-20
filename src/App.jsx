e05656ed94d3be6919da074ae6ed4c5ebda4de48
MarK22100 <fmzambrana3@gmail.com>
2024-04-20 20:38:20 -0300

    uniendo componentes a app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 
-import NavBar from "./components/Navbar/NavBar";
-import Socials from "./components/Navbar/Socials";
-import Footer from "./components/Footer/Footer";
+import NavBar from "./Components/Navbar/NavBar";
+import Socials from "./Components/Navbar/Socials";
+import Footer from "./Components/Footer/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Pages/Home";
-import Admin from "./components/Pages/Admin";
-import Error404 from "./components/Pages/Error404";
-import Contact from "./components/Pages/Contact";
-import CarouselFadeExample from "./components/CarouselFolder/Carousel";
-import ListProducts from "./components/Pages/product/listProduct";
-import FilterProduct from "./components/Pages/product/filterProduct";
+import Home from "./Components/Pages/Home";
+import Admin from "./Components/Pages/Admin";
+import Error404 from "./Components/Pages/Error404";
+import Contact from "./Components/Pages/Contact";
+import CarouselFadeExample from "./Components/CarouselFolder/Carousel";
+import ListProducts from "./Components/Pages/product/listProduct";
+import FilterProduct from "./Components/Pages/product/filterProduct";
 import { Col, Container, Row } from "react-bootstrap";
 
 
