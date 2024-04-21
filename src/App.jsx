a78ea394815ba237f0fd3dad2bf08358a23d475b
MarK22100 <fmzambrana3@gmail.com>
2024-04-21 15:27:08 -0300

    actualizacion de ramas

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import NavBar from "./Components/Navbar/NavBar";
 import Socials from "./Components/Navbar/Socials";
 import Footer from "./Components/Footer/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Components/Pages/Home";
 import Admin from "./Components/Pages/Admin";
 import Error404 from "./Components/Pages/Error404";
 import Contact from "./Components/Pages/Contact";
@@ -21,7 +20,7 @@ function App() {
         <main className=''>
         <CarouselFadeExample/>
           <Routes>
-            <Route path="/" element={<Home/>} />
+            <Route path="/" />
             <Route path="/contact" element={<Contact/>} />
             <Route path="/error" element={<Error404/>} />
             <Route path="/admin" element={<Admin/>} />
