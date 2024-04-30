92f6d7b0ae0a275652f3719abcde4dc84bae6c56
jemmert <jozemel5@gmail.com>
2024-04-30 03:04:24 -0300

    Solucionando conflictos de ramas

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -4,7 +4,34 @@ import UserContext from "../../Context/UserContext";
 import { useContext } from "react";
 
 const Home = () => {
-    const {currentUser}=useContext(UserContext);
+  const [products, setProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const API = import.meta.env.VITE_API;
+
+  const getProducts = async ()=>{
+    try {
+        const response =await axios.get(`${API}/products`)
+        setProducts(response.data);
+    } catch (error) {
+      console.log("Error en get axios---->", error);
+    }
+  };
+
+  useEffect(()=>{
+    getProducts();
+    return()=>{
+      setProducts([])
+    }
+  },[])
+
+  const handleCategorySelect = (categoria) => {
+    setSelectedCategory(categoria);
+  };
+
+  const filteredProducts = selectedCategory
+    ? products.filter((product) => product.category === selectedCategory)
+    : products;
 
     console.log("CURRENT USER-->", currentUser);
     return (
