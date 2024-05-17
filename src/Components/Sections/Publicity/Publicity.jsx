0d64c045a34618e468133cd90c6b933cd8d623c9
MarK22100 <fmzambrana3@gmail.com>
2024-05-16 22:11:48 -0300

    agregando publicidad

--- src/Components/Sections/Publicity/Publicity.jsx
+++ src/Components/Sections/Publicity/Publicity.jsx
@@ -1,4 +1,7 @@
-import banner1 from '../../CarouselFolder/banner1.webp';
+import banner1 from '../../Images/pub1.jpg';
+import banner2 from '../../Images/pub2.jpg';
+import banner3 from '../../Images/pub3.jpg';
+
 import publicity1 from '../../Images/900w-KB4TX5Jf9I4.webp';
 import publicity2 from '../../Images/publicity2.webp';
 import publicity3 from '../../Images/pexels-olenkabohovyk-3819969.jpg';
@@ -9,14 +12,14 @@ const Publicity = () => {
     return (
       <>
         <Carousel className="my-4 d-none d-md-block">
-          <Carousel.Item interval={1000}>
+          <Carousel.Item interval={3000}>
             <img src={banner1} alt="First slide" className="imgback" />
           </Carousel.Item>
-          <Carousel.Item interval={500}>
-            <img src={banner1} alt="First slide" className="imgback" />
+          <Carousel.Item interval={3500}>
+            <img src={banner2} alt="First slide" className="imgback" />
           </Carousel.Item>
           <Carousel.Item>
-            <img src={banner1} alt="First slide" className="imgback" />
+            <img src={banner3} alt="First slide" className="imgback" />
           </Carousel.Item>
         </Carousel>
 
@@ -24,13 +27,13 @@ const Publicity = () => {
           className="my-4 d-none d-md-block"
           style={{ height: "32.5em" }}
         >
-          <Carousel.Item interval={1000}>
+          <Carousel.Item interval={4000}>
             <img src={publicity1} alt="First slide" className="imgback" />
           </Carousel.Item>
-          <Carousel.Item interval={500}>
+          <Carousel.Item interval={4000}>
             <img src={publicity2} alt="First slide" className="imgback" />
           </Carousel.Item>
-          <Carousel.Item>
+          <Carousel.Item interval={4000}>
             <img src={publicity3} alt="First slide" className="imgback" />
           </Carousel.Item>
         </Carousel>
