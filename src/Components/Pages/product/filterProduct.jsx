7ff51f97b31f3f67900a2d1e06b2712c0bd13651
MarK22100 <fmzambrana3@gmail.com>
2024-05-16 21:49:11 -0300

    cambios finales publicidad y categorias

--- src/Components/Pages/product/filterProduct.jsx
+++ src/Components/Pages/product/filterProduct.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import "../product/productStyle.css"
-import { Button, Collapse } from 'react-bootstrap';
+import {Collapse } from 'react-bootstrap';
 
 // eslint-disable-next-line react/prop-types
 const FilterProduct = ({ onCategorySelect }) => {
   const [open, setOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
   
   const handleButtonClick = (category) => {
+    setSelectedCategory(category);
     if (category === 'Todos') {
       onCategorySelect(null);
     } else {
@@ -16,26 +18,25 @@ const FilterProduct = ({ onCategorySelect }) => {
 
   return (
     <>
-      <Button
-        className='btnCategory w-100'
+      <button
+        className='btnCategory w-100 '
         onClick={() => setOpen(!open)}
         aria-controls="example-collapse-text"
         aria-expanded={open}
-        style={{backgroundColor:'var(--fourth)', border:0, fontSize:'1.5rem', borderBottomLeftRadius: '30px', color:'#111', fontWeight:'700'}}
+       
       >
         CATEGORIAS
-      </Button>
+      </button>
       <Collapse in={open}>
         <div id="example-collapse-text">
           {['Hamburguesas', 'Ensaladas', 'Bebidas', 'Postres', 'Todos'].map((category, index) => (
-            <Button
+            <button
               key={index}
               onClick={() => handleButtonClick(category)}
-              className='flex-column d-flex w-75 mx-auto my-2'
-              style={{backgroundColor:'var(--fourth)', border:'0', fontSize:'1rem', fontWeight:'500', color:'#111'}}
+              className={`flex-column d-flex w-75 mx-auto my-2 selectCategory ${selectedCategory === category ? 'selected' : ''}`}
             >
               {category}
-            </Button>
+            </button>
           ))}
         </div>
       </Collapse>
--- src/Components/Sections/Publicity/Publicity.jsx
+++ src/Components/Sections/Publicity/Publicity.jsx
@@ -1,21 +1,40 @@
 import banner1 from '../../CarouselFolder/banner1.webp';
+import publicity1 from '../../Images/900w-KB4TX5Jf9I4.webp';
+import publicity2 from '../../Images/publicity2.webp';
+import publicity3 from '../../Images/pexels-olenkabohovyk-3819969.jpg';
 import { Carousel } from 'react-bootstrap';
+
 
 const Publicity = () => {
     return (
-        <Carousel 
-        className='my-4 d-sm-none d-md-block'
+      <>
+        <Carousel className="my-4 d-none d-md-block">
+          <Carousel.Item interval={1000}>
+            <img src={banner1} alt="First slide" className="imgback" />
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+            <img src={banner1} alt="First slide" className="imgback" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={banner1} alt="First slide" className="imgback" />
+          </Carousel.Item>
+        </Carousel>
+
+        <Carousel
+          className="my-4 d-none d-md-block"
+          style={{ height: "32.5em" }}
         >
-        <Carousel.Item interval={1000}>
-        <img src={banner1} alt="First slide" className='imgback' />
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-        <img src={banner1} alt="First slide" className='imgback' />
-        </Carousel.Item>
-        <Carousel.Item>
-        <img src={banner1} alt="First slide" className='imgback' />
-        </Carousel.Item>
-      </Carousel>
+          <Carousel.Item interval={1000}>
+            <img src={publicity1} alt="First slide" className="imgback" />
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+            <img src={publicity2} alt="First slide" className="imgback" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={publicity3} alt="First slide" className="imgback" />
+          </Carousel.Item>
+        </Carousel>
+      </>
     );
 };
 
